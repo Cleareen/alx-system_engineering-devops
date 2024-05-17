220f18a2683ece55a065520bc8f9c5eb702ec30f
EDDIE MKANSI <51905275+Cleareen@users.noreply.github.com>
2024-05-17 05:44:32 +0200

    Add files via upload

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
-# fix our stack so that we get to 0 errors
-exec { 'file limit':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+file { 'replace last line':
+    ensure  => present,
+    path    => '/etc/default/nginx',
+    content => 'ULIMIT="-n 4096"',
 }
+
+service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/default/nginx']
+}
+#/etc/default/nginx -- ULIMIT="-n 15" to ULIMIT="-n 4096"
